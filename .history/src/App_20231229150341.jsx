f2bb76f74b1c5e4e5109ab45129e0b3e4e6879d6
sarcosponce <sarcos@unsa.edu.pe>
2023-12-29 15:22:39 -0500

    add nextui

--- .history/src/App_20231229150341.jsx
+++ .history/src/App_20231229150341.jsx
@@ -6,9 +6,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
       </h1>
-      <Button color="primary" >
-        boton
-      </Button>
+      
     </>
   )
 }
--- .history/src/App_20231229150343.jsx
+++ .history/src/App_20231229150343.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
       </h1>
-      <Button color="primary" >
+      <Button>
         boton
       </Button>
     </>
